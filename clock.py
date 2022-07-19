2c2b7a0ccceeaf0eec9056a363724a92f9b0352d
metflekx <dogfuelx@gmail.com>
2022-07-20 02:18:06 +0300

    added refrence

--- clock.py
+++ clock.py
@@ -1,3 +1,11 @@
+# from the book "Think Python second edition"
+# author : Allen Downey
+
+# "Exercise 5.1. The time module provides a function, also named time, that returns the current
+# Greenwich Mean Time in “the epoch”, which is an arbitrary time used as a reference point. On
+# UNIX systems, the epoch is 1 January 1970."
+
+
 import time
 import math
 
